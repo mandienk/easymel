7c8ec8a6469ec5b53aa4e4cb4dd09322595b7831
mandien <m.kakez@gmail.com>
2017-04-17 07:02:56 +0200

    comments traduction

--- example-basic.php
+++ example-basic.php
@@ -17,27 +17,27 @@ $obj->values = $result[0];
 
 echo getMyForm($obj, $db);
 
-/*
- * This function create the form from the database table
+/*	 
+ * Function getFormFromTable () called in getMyForm ()
  *
- * array	$arData	Les valeurs des champs
- * array	$arError Les erreurs de saisie (facultatif)
- * string	$databaseNane Le nom de la base de données (facultatif)
- * string	$tableName Le nom de la table (facultatif)
- * array	$arNoFields les champs à ignorer (facultatif)
- * array	$arMapFields Le libellé des champs (facultatif)
- * array	$arComboFields les listes déroulantes
- * array	$arRelationFields les relations entre champs
- * array	$arDisabledFields les champs désactivés
- * boolean	$disabled true/false permet de désactiver tous les champs du formulaire
- * string	$plus permet l'ajout d'un message, lien, etc.
+ * array	$arData	The field values
+ * array	$arError Input errors (optional)
+ * string	$databaseNane the database name (optional)
+ * string	$tableName the database table name
+ * array	$arNoFields the fields to ignore
+ * array	$arMapFields the field labels (optional)
+ * array	$arComboFields the drop-down lists (optional)
+ * array	$arRelationFields the field relations : NOT IMPLEMENTED!
+ * array	$arDisabledFields the disabled fields
+ * boolean	$disabled true/false allow to disable all the form fields
+ * string	$plus allow to add a message, link, etc.
  */
 function getMyForm($obj, $db)
 {
 	// We build the form
 	$easy = new _easyMFormGen($db);
-	return $easy->getFormFromTable((isset($obj->values)? $obj->values : null),
-			(isset($obj->arErrorsProduit)?(object)$obj->arErrorsProduit:null), // Convert error array to object
+	return $easy->getFormFromTable((isset($obj->values)? $obj->values : null), // field values
+			(isset($obj->arErrorsProduit)?(object)$obj->arErrorsProduit:null), // Errors : Convert error array to object
 			'test', // database
 			'users', // database table
 			array('user_id'), // Fields to hide
